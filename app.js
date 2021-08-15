53e3704de9010927d3f86fb3765e7678576a9b39
Devendra Pandey <devenkshp@gmail.com>
2021-08-15 12:08:50 +0545

    Add documentation

--- app.js
+++ app.js
@@ -1,3 +1,143 @@
+// Class to create fan object
+class Fan {
+  /**
+   * @constructor
+   * @param {HTMLElement} address - IP address of the fan
+   * @param {HTMLElement} pwrBtnLbl - Label of the power button on UI
+   * @param {HTMLElement} fanStateIndicator - Element which shows current state of the fan on UI
+   * @param {HTMLElement} speedIndicator - Element which shows speed of the fan on UI
+   * @param {HTMLElement} swingIndicator - Element which show wherether the fan is swinging or not on UI
+   * @param {HTMLElement} timerIndicator - Element which shows timer state on UI
+   * @param {HTMLElement} dialog - Element to show error when failed to connect to the fan
+   */
+  constructor(
+    address,
+    pwrBtnLbl,
+    fanStateIndicator,
+    speedIndicator,
+    swingIndicator,
+    timerIndicator,
+    dialog
+  ) {
+    this.address = address;
+    this.pwrBtnLbl = pwrBtnLbl;
+    this.fanState = fanStateIndicator;
+    this.speedIndicator = speedIndicator;
+    this.swingIndicator = swingIndicator;
+    this.timerIndicator = timerIndicator;
+    this.dialog = dialog;
+
+    this.state = {
+      loading: true,
+      connected: false,
+      error: false,
+      fanOn: 0,
+      speed: 0,
+      swing: 0,
+    };
+  }
+
+  /**
+   * Update state and UI
+   * @param {Object} newState - State to set
+   */
+  setState(newState) {
+    this.state = Object.assign(this.state, newState);
+
+    // Update the UI after setting new state
+    this.updateUi();
+  }
+
+  // Check whether the connection to the fan is established or not
+  connect() {
+    this.setState({ loading: true, error: false });
+    fetch(this.address)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ ...json, loading: false });
+      })
+      .catch((e) => {
+        this.setState({ error: true, loading: false });
+      });
+  }
+
+  /**
+   * Toggle the fan ON/OFF
+   * @param {String} cmd - "on" or "off"
+   */
+  turn(cmd) {
+    fetch(`${this.address}/${cmd}`)
+      .then((res) => res.json())
+      .then((json) => this.setState(json))
+      .catch((e) => this.setState({ error: true }));
+  }
+
+  /**
+   * Toggle the swing
+   * @param {string} cmd - "On" or "Off"
+   */
+  swing(cmd) {
+    fetch(`${this.address}/swing${cmd}`)
+      .then((res) => res.json())
+      .then((json) => this.setState(json))
+      .catch((e) => this.setState({ error: true }));
+  }
+
+  /**
+   * Handle the fan speed
+   * @param {string} speed - <One/Two/Three>
+   */
+  speed(speed) {
+    fetch(`${this.address}/speed${speed}`)
+      .then((res) => res.json())
+      .then((json) => this.setState(json))
+      .catch((e) => this.setState({ error: true }));
+  }
+
+  // Function to update UI on every state change
+  updateUi() {
+    !this.state.loading && this.state.error
+      ? this.dialog.show()
+      : this.dialog.close();
+
+    this.pwrBtnLbl.style.background = this.state.fanOn
+      ? "url(./assets/power-on.svg)"
+      : "url(./assets/power-off.svg)";
+
+    this.fanState.innerText = this.state.error
+      ? "n/a"
+      : this.state.fanOn
+      ? "On"
+      : "Off";
+
+    this.fanState.style.color = this.state.error
+      ? "#505050"
+      : this.state.fanOn
+      ? "#00CE00"
+      : "red";
+
+    this.swingIndicator.innerText = this.state.error
+      ? "n/a"
+      : this.state.swing
+      ? "On"
+      : "Off";
+    this.swingIndicator.style.color = this.state.error
+      ? "#505050"
+      : this.state.swing
+      ? "#00CE00"
+      : "red";
+
+    this.speedIndicator.innerText =
+      this.state.speed == 1
+        ? "one"
+        : this.state.speed == 2
+        ? "two"
+        : this.state.speed == 3
+        ? "three"
+        : "n/a";
+  }
+}
+
 window.onload = () => {
   // Select all the UI elements
   const fanStateIndicator = document.getElementById("fan-state-indicator"),
@@ -16,10 +156,10 @@ window.onload = () => {
     setIpBtn = document.getElementById("set-ip-btn"),
     currentIp = document.getElementById("current-ip"),
     snackbar = document.getElementById("snackbar"),
-    dialog = document.getElementById("dialog"),
-    reconnectBtn = document.getElementById("reconnect-btn");
+    reconnectBtn = document.getElementById("reconnect-btn"),
+    dialog = document.getElementsByTagName("dialog")[0];
 
-  // Read from local storage
+  // Read fan IP address from local storage
   let fanIp = localStorage.getItem("fanIp");
 
   // If IP is not set, open the modal
@@ -29,7 +169,7 @@ window.onload = () => {
     currentIp.innerText = fanIp + "/";
   }
 
-  // Show modal on clicking seetting button
+  // Show the modal on clicking setting button
   settingBtn.addEventListener("click", (e) => {
     modal.style.display = "flex";
   });
@@ -71,8 +211,8 @@ window.onload = () => {
   // Connect to the fan
   fan.connect();
 
-  // Add listeners to the speed button
-  pwrBtn.addEventListener("click", (e) => {
+  // Add listeners to the buttons
+  pwrBtn.addEventListener("click", () => {
     if (!fanIp) modal.style.display = "flex";
     fan.state.connected && !fan.state.fanOn ? fan.turn("on") : fan.turn("off");
     fan.state.error
@@ -86,158 +226,49 @@ window.onload = () => {
 
   speedOneBtn.addEventListener("click", () => {
     if (!fan.state.fanOn)
-      showSnack(snackbar, "alert", "Turn on the fan first.");
+      return showSnack(snackbar, "alert", "Turn on the fan first.");
     fan.state.fanOn ? fan.speed("One") : null;
   });
 
   speedTwoBtn.addEventListener("click", () => {
     if (!fan.state.fanOn)
-      showSnack(snackbar, "alert", "Turn on the fan first.");
+      return showSnack(snackbar, "alert", "Turn on the fan first.");
     fan.state.fanOn ? fan.speed("Two") : null;
   });
 
   speedThreeBtn.addEventListener("click", () => {
     if (!fan.state.fanOn)
-      showSnack(snackbar, "alert", "Turn on the fan first.");
+      return showSnack(snackbar, "alert", "Turn on the fan first.");
     fan.state.fanOn ? fan.speed("Three") : null;
   });
 
-  swingBtn.addEventListener("click", (e) => {
+  swingBtn.addEventListener("click", () => {
     if (!fan.state.fanOn)
-      showSnack(snackbar, "alert", "Turn on the fan first.");
+      return showSnack(snackbar, "alert", "Turn on the fan first.");
     fan.state.fanOn && !fan.state.swing ? fan.swing("On") : fan.swing("Off");
   });
 
   reconnectBtn.addEventListener("click", () => fan.connect());
 };
 
-class Fan {
-  constructor(
-    address,
-    pwrBtnLbl,
-    fanStateIndicator,
-    speedIndicator,
-    swingIndicator,
-    timerIndicator,
-    dialog
-  ) {
-    this.state = {
-      loading: true,
-      connected: false,
-      error: false,
-      fanOn: 0,
-      speed: 0,
-      swing: 0,
-    };
-    this.address = address;
-    this.pwrBtnLbl = pwrBtnLbl;
-    this.fanState = fanStateIndicator;
-    this.speedIndicator = speedIndicator;
-    this.swingIndicator = swingIndicator;
-    this.timerIndicator = timerIndicator;
-    this.dialog = dialog;
-  }
-
-  // Update state and update UI
-  setState(newState) {
-    this.state = Object.assign(this.state, newState);
-
-    // Upadate the UI
-    this.updateUi();
-  }
-
-  // Check whether the connection to the fan is established or not
-  connect() {
-    this.setState({ loading: true });
-    fetch(this.address)
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState(json);
-      })
-      .catch((e) => {
-        this.setState({ error: true });
-      })
-      .finally(() => {
-        this.setState({ loading: false });
-        this.updateUi();
-      });
-  }
-
-  /**
-   * Toggle the fan ON/OFF
-   * @cmd "on"/"off"
-   */
-  turn(cmd) {
-    fetch(`${this.address}/${cmd}`)
-      .then((res) => res.json())
-      .then((json) => this.setState(json))
-      .catch((e) => this.setState({ error: true }));
-  }
-
-  swing(cmd) {
-    fetch(`${this.address}/swing${cmd}`)
-      .then((res) => res.json())
-      .then((json) => this.setState(json))
-      .catch((e) => this.setState({ error: true }));
-  }
-
-  // Handle speed
-  speed(speed) {
-    fetch(`${this.address}/speed${speed}`)
-      .then((res) => res.json())
-      .then((json) => this.setState(json))
-      .catch((e) => this.setState({ error: true }));
-  }
-
-  // Function to update UI
-  updateUi() {
-    this.dialog.style.visibility =
-      !this.state.loading && this.state.error ? "visible" : "hidden";
-    this.pwrBtnLbl.className = this.state.fanOn ? "pwr-off" : "pwr-on";
-    this.fanState.innerText = this.state.error
-      ? "n/a"
-      : this.state.fanOn
-      ? "On"
-      : "Off";
-
-    this.fanState.style.color = this.state.error
-      ? "#505050"
-      : this.state.fanOn
-      ? "#00CE00"
-      : "red";
-
-    this.swingIndicator.innerText = this.state.error
-      ? "n/a"
-      : this.state.swing
-      ? "On"
-      : "Off";
-    this.swingIndicator.style.color = this.state.error
-      ? "#505050"
-      : this.state.swing
-      ? "#00CE00"
-      : "red";
-
-    this.speedIndicator.innerText =
-      this.state.speed == 1
-        ? "one"
-        : this.state.speed == 2
-        ? "two"
-        : this.state.speed == 3
-        ? "three"
-        : "n/a";
-  }
-}
-
-// Validate IP address
+/**
+ * Function for validatting IP address
+ * @param {ip} ip - IP address of the fan entered by the user
+ * @returns {boolean} - True if valid IP address is entered else False
+ */
 function validateIp(ip) {
   const regex = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
-  if (regex.test(ip)) return true;
-  else return false;
+  return regex.test(ip);
 }
 
-// Show snack
-let timer;
+/**
+ *
+ * @param {HTMLElement} snack - Snack element
+ * @param {string} type - Type of the snackbar
+ * @param {string} message - Message to display
+ */
 function showSnack(snack, type, message) {
+  let timer;
   snack.innerText = message;
   snack.className = type;
 
